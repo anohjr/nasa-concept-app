e35c039250cfa9616f494b359fbead796ce4804d
anohjr <rosenaomi.hj@gmail.com>
2023-04-13 00:23:11 +0200

    ajout diamond-svg et mis en place sur la nav bar

--- frontend/src/components/nav_bar.jsx
+++ frontend/src/components/nav_bar.jsx
@@ -1,21 +1,23 @@
+import diamond from "../assets/diamond.svg";
+
 function NavBar() {
   return (
     <div id="navbar_container">
       <div id="navtitle_container">
         <div id="nav_topics" className="section_container">
-          <div className="losange_vector" />
+          <img className="diamond_svg" src={diamond} alt="diamond logo svg" />
           <h4 className="nav_title">Topics</h4>
         </div>
         <div id="nav_gallery" className="section_container">
-          <div className="losange_vector" />
+          <img className="diamond_svg" src={diamond} alt="diamond logo svg" />
           <h4 className="nav_title">Gallery</h4>
         </div>
         <div id="nav_solar" className="section_container">
-          <div className="losange_vector" />
+          <img className="diamond_svg" src={diamond} alt="diamond logo svg" />
           <h4 className="nav_title">Solar System</h4>
         </div>
         <div id="nav_ISS" className="section_container">
-          <div className="losange_vector" />
+          <img className="diamond_svg" src={diamond} alt="diamond logo svg" />
           <h4 className="nav_title">ISS Tracker</h4>
         </div>
       </div>
