739a0d88d26680668e7c0bf451c4897fdba9d496
JeanMMillet <millet.jm@live.com>
2023-04-14 10:01:54 +0200

    Change button with cross icon

--- frontend/src/components/GalleryHeader.jsx
+++ frontend/src/components/GalleryHeader.jsx
@@ -1,20 +1,21 @@
 import nasaLogo from "../assets/nasa-logo.png";
 
-// import closeBttn from "../assets/close-button.png";
+import closeBttn from "../assets/close-button.png";
 
 function GalleryHeader() {
   function closeGallery() {
     document.getElementsByClassName("home")[0].style.display = "none";
     document.getElementById("open").style.display = "initial";
   }
+  document
+    .getElementById("closeButton")
+    .addEventListener("click", closeGallery);
   return (
     <div id="header">
       <img src={nasaLogo} alt="LOGO NASA" id="nasaLogo" />
       <h1>Image Gallery</h1>
-      <button onClick={closeGallery} type="button">
-        Close
-      </button>
-      {/* <img src={closeBttn} alt="" id="closeButton" /> */}
+
+      <img src={closeBttn} alt="" id="closeButton" />
     </div>
   );
 }
