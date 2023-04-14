790dda11d983cd1b4ec4faf4f96330157695d5c9
JeanMMillet <millet.jm@live.com>
2023-04-14 11:06:43 +0200

    comment button

---
+++ frontend/src/components/GalleryHeader.jsx
@@ -0,0 +1,24 @@
+import nasaLogo from "../assets/nasa-logo.png";
+
+// import closeBttn from "../assets/close-button.png";
+
+function GalleryHeader() {
+  function closeGallery() {
+    document.getElementsByClassName("home")[0].style.display = "none";
+
+    document.getElementById("open").style.display = "initial";
+  }
+  document
+    .getElementById("closeButton")
+    .addEventListener("click", closeGallery);
+  return (
+    <div id="header">
+      <img src={nasaLogo} alt="LOGO NASA" id="nasaLogo" />
+      <h1>Image Gallery</h1>
+
+      {/* <img src={closeBttn} alt="" id="closeButton" /> */}
+    </div>
+  );
+}
+
+export default GalleryHeader;
