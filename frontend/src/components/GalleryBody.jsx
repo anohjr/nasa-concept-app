f09f3cf54ac9fb740940bdbe2dc2ee2763a56da0
JeanMMillet <millet.jm@live.com>
2023-04-28 11:24:38 +0200

    delete comments and unnecessary files

--- frontend/src/components/GalleryBody.jsx
+++ frontend/src/components/GalleryBody.jsx
@@ -3,13 +3,6 @@ import { useState } from "react";
 import ImageSwiper from "./ImageSwiper";
 import ImageGrid from "./ImageGrid";
 
-// import search from "../assets/search";
-
-// GmJrWvexeBC4z79UNrwhWg6RCtDfQFq5DCP5qiBh
-// https://pokebuildapi.fr/api/v1/pokemon/limit/100
-// https://images-api.nasa.gov/media_type?=image
-// https://api.nasa.gov/planetary/apod?api_key=GmJrWvexeBC4z79UNrwhWg6RCtDfQFq5DCP5qiBh
-
 function GalleryBody({ data, setPageSize, setPageNum }) {
   const [visible, setVisible] = useState(false);
   const [image, setImage] = useState("");
