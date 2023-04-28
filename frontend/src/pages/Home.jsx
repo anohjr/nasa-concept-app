6e29267a24ff7e3a324e24becb213100d160a456
JeanMMillet <millet.jm@live.com>
2023-04-28 11:30:22 +0200

    delete doublon nasa logo

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,11 @@
 import Sidebar from "../components/Sidebar";
-import LogoNasa from "../components/logoNASA";
+
 import Button from "../components/Button_iotd";
 
 function Home() {
   return (
     <div id="Home_Page">
       <Sidebar />
-      <LogoNasa />
       <Button />
     </div>
   );
