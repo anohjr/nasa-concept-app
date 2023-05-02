fc094842b129bb865e3ef4af7e3017f7d66239e4
etienne chamarier <etienne.chamarier@gmail.com>
2023-05-02 10:13:34 +0200

    ne pas prendre en compte

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,55 +1,61 @@
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable import/no-extraneous-dependencies */
-import { useState } from "react";
-import { CSSTransition, SwitchTransition } from "react-transition-group";
-// import { Transition } from "react-transition-group";
-// --> Components
-import NavBar from "./components/nav_bar";
-import LogoNasa from "./components/logoNASA";
-import Gallery from "./components/Gallery";
-import IssTitle from "./components/IssTitle";
-// --> PAGES
-import SolarSystem from "./pages/SolarSystem";
+// /* eslint-disable no-restricted-syntax */
+// /* eslint-disable import/no-extraneous-dependencies */
 // import { useState } from "react";
-// import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Iss from "./pages/ISS";
+// import { CSSTransition, SwitchTransition } from "react-transition-group";
+// // import { Transition } from "react-transition-group";
+// // --> Components
+// import NavBar from "./components/nav_bar";
+// import LogoNasa from "./components/logoNASA";
+// import Gallery from "./components/Gallery";
+// import IssTitle from "./components/IssTitle";
+// // --> PAGES
+// import SolarSystem from "./pages/SolarSystem";
+// // import { useState } from "react";
+// // import { Route, Routes } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Iss from "./pages/ISS";
+// // import { Route, Routes } from "react-router-dom";
 
-// import ImageOfTheDay from "./pages/ImageOfTheDay";
-import "./App.css";
-// import Sidebar from "./components/Sidebar";
+// // import ImageOfTheDay from "./pages/ImageOfTheDay";
+// import "./App.css";
+// // import Sidebar from "./components/Sidebar";
 
-function App() {
-  const [page, setPage] = useState("Home");
-  const [displayGallery, setDisplayGallery] = useState(false);
-  console.log(page);
+// function App() {
+//   const [page, setPage] = useState("Home");
+//   const [displayGallery, setDisplayGallery] = useState(false);
+//   console.log(page);
 
-  return (
-    <>
-      <div id="app_overflow">
-        <Gallery display={displayGallery} setDisplay={setDisplayGallery} />
-        <NavBar
-          loadPage={setPage}
-          currentPage={page}
-          setDisplayGallery={setDisplayGallery}
-          display={displayGallery}
-        />
-      </div>
-      <LogoNasa displayGallery={displayGallery} />
-      <IssTitle display={page} />
-      <SwitchTransition>
-        <CSSTransition key={page} timeout={350} classNames="page">
-          {
-            {
-              ISS: <Iss displayGallery={displayGallery} />,
-              SolarSystem: <SolarSystem displayGallery={displayGallery} />,
-              Home: <Home />,
-            }[page]
-          }
-        </CSSTransition>
-      </SwitchTransition>
-    </>
-  );
-}
+//   return (
+//     // <Routes>
+//     //   <Route path="/" element={<Home />} />
+//     //   <Route path="/iss" element={<Iss />} />
+//     //   <Route path="/solar-system" element={<SolarSystem />} />
+//     //   <Route path="/gallery" element={<Gallery />} />
 
-export default App;
+//       // <div id="app_overflow">
+//         <Gallery display={displayGallery} setDisplay={setDisplayGallery} />
+//         <NavBar
+//           loadPage={setPage}
+//           currentPage={page}
+//           setDisplayGallery={setDisplayGallery}
+//           display={displayGallery}
+//         />
+//       </div>
+//       <LogoNasa displayGallery={displayGallery} />
+//       <IssTitle display={page} />
+//       <SwitchTransition>
+//         <CSSTransition key={page} timeout={350} classNames="page">
+//           {
+//             {
+//               ISS: <Iss displayGallery={displayGallery} />,
+//               SolarSystem: <SolarSystem displayGallery={displayGallery} />,
+//               Home: <Home />,
+//             }[page]
+//           }
+//         </CSSTransition>
+//       </SwitchTransition>
+//     </Routes>
+//   );
+// }
+
+// export default App;
--- frontend/src/components/nav_bar.jsx
+++ frontend/src/components/nav_bar.jsx
@@ -1,84 +1,91 @@
-/* eslint-disable no-nested-ternary */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable react/prop-types */
-/* eslint-disable react/destructuring-assignment */
-import diamondFull from "../assets/diamondFull.svg";
-import diamond from "../assets/diamond.svg";
+// /* eslint-disable no-nested-ternary */
+// /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+// /* eslint-disable jsx-a11y/click-events-have-key-events */
+// /* eslint-disable react/prop-types */
+// /* eslint-disable react/destructuring-assignment */
+// import diamondFull from "../assets/diamondFull.svg";
+// import diamond from "../assets/diamond.svg";
+// // import { Link } from "react-router-dom";
 
-function NavBar(props) {
-  return (
-    <div
-      id="navbar_container"
-      style={
-        props.display
-          ? { bottom: "-20%", transitionDelay: "0ms" }
-          : { bottom: "0", transitionDelay: "600ms" }
-      }
-    >
-      <div id="navtitle_container">
-        <div id="nav_topics" className="section_container">
-          <img
-            className="diamond_svg"
-            src={props.currentPage === "Home" ? diamondFull : diamond}
-            alt="diamond logo svg"
-          />
-          <h4 className="nav_title" onClick={() => props.loadPage("Home")}>
-            Home
-          </h4>
-        </div>
-        <div id="nav_gallery" className="section_container">
-          <img className="diamond_svg" src={diamond} alt="diamond logo svg" />
-          <h4
-            className="nav_title"
-            onClick={() => props.setDisplayGallery(true)}
-          >
-            Gallery
-          </h4>
-        </div>
-        <div id="nav_solar" className="section_container">
-          <img
-            className="diamond_svg"
-            src={props.currentPage === "SolarSystem" ? diamondFull : diamond}
-            alt="diamond logo svg"
-          />
-          <h4
-            className="nav_title"
-            onClick={() => props.loadPage("SolarSystem")}
-          >
-            Solar System
-          </h4>
-        </div>
-        <div id="nav_ISS" className="section_container">
-          <img
-            className="diamond_svg"
-            src={props.currentPage === "ISS" ? diamondFull : diamond}
-            alt="diamond logo svg"
-          />
-          <h4 className="nav_title" onClick={() => props.loadPage("ISS")}>
-            ISS Tracker
-          </h4>
-        </div>
-      </div>
-      <div id="bar_container">
-        <div
-          id="dynamic_bar"
-          className="section_bar"
-          style={{
-            left:
-              props.currentPage === "Home"
-                ? "0"
-                : props.currentPage === "SolarSystem"
-                ? "50%"
-                : props.currentPage === "ISS"
-                ? "75%"
-                : null,
-          }}
-        />
-        <div id="progression_bar" />
-      </div>
-    </div>
-  );
-}
+// function NavBar(props) {
+//   return (
+//     <div
+//       id="navbar_container"
+//       style={
+//         props.display
+//           ? { bottom: "-20%", transitionDelay: "0ms" }
+//           : { bottom: "0", transitionDelay: "600ms" }
+//       }
+//     >
+//       <div id="navtitle_container">
+//         <Link to="/">
+//           <div id="nav_topics" className="section_container">
+//             <img
+//               className="diamond_svg"
+//               src={props.currentPage === "Home" ? diamondFull : diamond}
+//               alt="diamond logo svg"
+//             />
+//             <h4 className="nav_title" onClick={() => props.loadPage("Home")}>
+//               Home
+//             </h4>
+//           </div>
+//         </Link>
+//         <Link to="/gallery">
+//           <div id="nav_gallery" className="section_container">
+//             <img className="diamond_svg" src={diamond} alt="diamond logo svg" />
+//             <h4
+//               className="nav_title"
+//               onClick={() => props.setDisplayGallery(true)}
+//             >
+//               Gallery
+//             </h4>
+//           </div>
+//         </Link>
+//         <Link to="/solar-system">
+//           <div id="nav_solar" className="section_container">
+//             <img
+//               className="diamond_svg"
+//               src={props.currentPage === "SolarSystem" ? diamondFull : diamond}
+//               alt="diamond logo svg"
+//             />
+//             <h4
+//               className="nav_title"
+//               onClick={() => props.loadPage("SolarSystem")}
+//             >
+//               Solar System
+//             </h4>
+//           </div>
+//         </Link>
+//         <div id="nav_ISS" className="section_container">
+//           <img
+//             className="diamond_svg"
+//             src={props.currentPage === "ISS" ? diamondFull : diamond}
+//             alt="diamond logo svg"
+//           />
+//           <h4 className="nav_title" onClick={() => props.loadPage("ISS")}>
+//             ISS Tracker
+//           </h4>
+//         </div>
+//       </div>
+//       <div id="bar_container">
+//         <div
+//           id="dynamic_bar"
+//           className="section_bar"
+//           style={{
+//             left:
+//               props.currentPage === "Home"
+//                 ? "0"
+//                 : props.currentPage === "SolarSystem"
+//                 ? "50%"
+//                 : props.currentPage === "ISS"
+//                 ? "75%"
+//                 : null,
+//           }}
+//         />
+//         <div id="progression_bar" />
+//       </div>
+//     </div>
+//   );
+// }
 
-export default NavBar;
+// export default NavBar;
