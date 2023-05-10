56ec598996f4a06a75cfe411784031dd0a766677
anohjr <rosenaomi.hj@gmail.com>
2023-05-10 09:57:06 +0200

    minor correction for ISS part and read me md updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,8 @@
-const path = require("path");
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
